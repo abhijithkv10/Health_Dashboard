c02712c791c875f02190f961a56f2aa21621b6e8
abhijith.kv <abhijithambadi10@gmail.com>
2026-06-28 19:36:43 +0530

    Remove S3 backend (use local state)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket = "aws-monitor-terraform-state"
-    key    = "prod/terraform.tfstate"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
